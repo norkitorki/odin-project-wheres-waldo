7bcc6d8f3783da13187b97a9152fe1415d3a8749
norkitorki <norki@tuta.io>
2026-01-22 13:45:26 +0100

    Clean up winningScreen render code

--- app/javascript/tests/components/WinningScreen.test.jsx
+++ app/javascript/tests/components/WinningScreen.test.jsx
@@ -2,45 +2,56 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import WinningScreen from '@javascript/components/WinningScreen/WinningScreen';
 import { act, render, screen } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+  vi,
+} from 'vitest';
 
 vi.mock('@javascript/utils.js', () => ({ _fetch: fetchMock }));
 
 let responseObject;
 const fetchMock = vi.hoisted(() => vi.fn(() => responseObject));
 const discoveriesResetMock = vi.hoisted(() => vi.fn());
-const map = { id: 1, name: 'Waldo Dreamland' };
-const discoveries = { reset: discoveriesResetMock };
-const newUser = {
-  form: {
-    inputs: {
-      name: {
-        type: 'text',
-        name: 'user[name]',
-        id: 'user_name',
-      },
-      submit: {
-        name: 'commit',
-        text: 'Create User',
-        type: 'submit',
-      },
-    },
-    extras: {
-      csrf: {
-        name: 'authenticity_token',
-        type: 'hidden',
-        defaultValue:
-          'ewfeflwefJTF8758weqfujqwdf7817364jkHIGDHJIQPQWDQWDGBQGWDW',
-        autoComplete: 'off',
-      },
-    },
+const props = {
+  map: { id: 1, name: 'Waldo Dreamland' },
+  discoveries: { reset: discoveriesResetMock },
+  newUser: {
     form: {
-      action: '/users',
-      acceptCharset: 'UTF-8',
-      method: 'post',
+      inputs: {
+        name: {
+          type: 'text',
+          name: 'user[name]',
+          id: 'user_name',
+        },
+        submit: {
+          name: 'commit',
+          text: 'Create User',
+          type: 'submit',
+        },
+      },
+      extras: {
+        csrf: {
+          name: 'authenticity_token',
+          type: 'hidden',
+          defaultValue:
+            'ewfeflwefJTF8758weqfujqwdf7817364jkHIGDHJIQPQWDQWDGBQGWDW',
+          autoComplete: 'off',
+        },
+      },
+      form: {
+        action: '/users',
+        acceptCharset: 'UTF-8',
+        method: 'post',
+      },
     },
+    formErrors: null,
   },
-  formErrors: null,
 };
 
 afterEach(() => vi.clearAllMocks());
@@ -52,43 +63,31 @@ beforeEach(() => {
 });
 
 test('fetches current and the best ten scores', async () => {
-  await act(() => {
-    render(
-      <WinningScreen map={map} discoveries={discoveries} newUser={newUser} />
-    );
-  });
+  await act(() => render(<WinningScreen {...props} />));
 
   expect(fetchMock).toHaveBeenCalledTimes(2);
   expect(fetchMock).toHaveBeenCalledWith('/score', 'GET');
   expect(fetchMock).toHaveBeenLastCalledWith(
-    `/scores?map_id=${map.id}&page=1&per_page=10`,
+    `/scores?map_id=${props.map.id}&page=1&per_page=10`,
     'GET',
-    expect.any(Function)
+    expect.any(Function),
   );
 });
 
 test('renders final score', async () => {
-  await act(() => {
-    render(
-      <WinningScreen map={map} discoveries={discoveries} newUser={newUser} />
-    );
-  });
+  await act(() => render(<WinningScreen {...props} />));
 
   expect(
-    screen.getByText(/you've cleared the map with a score of/i)
+    screen.getByText(/you've cleared the map with a score of/i),
   ).toBeInTheDocument();
   expect(screen.getByText(/900\.24/i)).toBeInTheDocument();
 });
 
 test('renders navigation buttons', async () => {
-  await act(() => {
-    render(
-      <WinningScreen map={map} discoveries={discoveries} newUser={newUser} />
-    );
-  });
+  await act(() => render(<WinningScreen {...props} />));
 
   expect(
-    await screen.findByRole('button', { name: /replay/i })
+    await screen.findByRole('button', { name: /replay/i }),
   ).toBeInTheDocument();
   expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
   expect(screen.getByRole('link', { name: /map scores/i })).toBeInTheDocument();
@@ -98,11 +97,7 @@ describe('when clicking replay button', () => {
   test('sends calls to reset score/discoveries', async () => {
     const user = userEvent.setup();
 
-    await act(() => {
-      render(
-        <WinningScreen map={map} discoveries={discoveries} newUser={newUser} />
-      );
-    });
+    await act(() => render(<WinningScreen {...props} />));
 
     const replayButton = screen.getByRole('button', { name: /replay/i });
     await user.click(replayButton);
@@ -112,14 +107,36 @@ describe('when clicking replay button', () => {
   });
 });
 
+describe('when user is signed out', () => {
+  test('renders user form', async () => {
+    await act(() => render(<WinningScreen {...props} />));
+
+    const input = screen.getByRole('textbox', {
+      name: /enter your name to save your result/i,
+    });
+    const submitButton = screen.getByRole('button', { name: /create user/i });
+    const form = submitButton.form;
+
+    expect(input).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+    expect(form.action).toMatch(/\users$/i);
+    expect(form.method).toBe('post');
+  });
+});
+
 describe('when user is signed in', () => {
+  let newUser;
+
+  beforeAll(() => {
+    newUser = props.newUser;
+    props.newUser = null;
+  });
+
+  afterAll(() => (props.newUser = newUser));
+
   describe('when no previous score has been saved', () => {
     test('posts score', async () => {
-      await act(() => {
-        render(
-          <WinningScreen map={map} discoveries={discoveries} newUser={null} />
-        );
-      });
+      await act(() => render(<WinningScreen {...props} />));
 
       expect(fetchMock).toHaveBeenCalledWith('/scores', 'POST');
     });
@@ -133,11 +150,7 @@ describe('when user is signed in', () => {
       };
       fetchMock.mockResolvedValueOnce(responseObject);
 
-      await act(() => {
-        render(
-          <WinningScreen map={map} discoveries={discoveries} newUser={null} />
-        );
-      });
+      await act(() => render(<WinningScreen {...props} />));
 
       expect(fetchMock).toHaveBeenCalledWith('/scores', 'POST');
     });
@@ -149,34 +162,9 @@ describe('when user is signed in', () => {
       };
       fetchMock.mockResolvedValueOnce(responseObject);
 
-      await act(() => {
-        render(
-          <WinningScreen map={map} discoveries={discoveries} newUser={null} />
-        );
-      });
+      await act(() => render(<WinningScreen {...props} />));
 
       expect(fetchMock).not.toHaveBeenCalledWith('/scores', 'POST');
     });
-  });
-});
-
-describe('when user is signed out', () => {
-  test('renders user form', async () => {
-    await act(() => {
-      render(
-        <WinningScreen map={map} discoveries={discoveries} newUser={newUser} />
-      );
-    });
-
-    const input = screen.getByRole('textbox', {
-      name: /enter your name to save your result/i,
-    });
-    const submitButton = screen.getByRole('button', { name: /create user/i });
-    const form = submitButton.form;
-
-    expect(input).toBeInTheDocument();
-    expect(submitButton).toBeInTheDocument();
-    expect(form.action).toMatch(/\users$/i);
-    expect(form.method).toBe('post');
   });
 });
