59228e599d240420deb146eb2c72b41a5530c44a
norkitorki <norki@tuta.io>
2026-01-24 07:09:26 +0100

    Update rails version to 8.1.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_24_060516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,8 +19,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
     t.bigint "map_id"
     t.float "x"
     t.float "y"
-    t.index [ "findable_id" ], name: "index_coordinates_on_findable_id"
-    t.index [ "map_id" ], name: "index_coordinates_on_map_id"
+    t.index ["findable_id"], name: "index_coordinates_on_findable_id"
+    t.index ["map_id"], name: "index_coordinates_on_map_id"
   end
 
   create_table "findables", force: :cascade do |t|
@@ -33,8 +33,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
   create_table "findables_maps", id: false, force: :cascade do |t|
     t.bigint "findable_id"
     t.bigint "map_id"
-    t.index [ "findable_id" ], name: "index_findables_maps_on_findable_id"
-    t.index [ "map_id" ], name: "index_findables_maps_on_map_id"
+    t.index ["findable_id"], name: "index_findables_maps_on_findable_id"
+    t.index ["map_id"], name: "index_findables_maps_on_map_id"
   end
 
   create_table "maps", force: :cascade do |t|
@@ -49,8 +49,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.float "value"
-    t.index [ "map_id" ], name: "index_scores_on_map_id"
-    t.index [ "user_id" ], name: "index_scores_on_user_id"
+    t.index ["map_id"], name: "index_scores_on_map_id"
+    t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -58,8 +58,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
     t.text "data"
     t.string "session_id", null: false
     t.datetime "updated_at", null: false
-    t.index [ "session_id" ], name: "index_sessions_on_session_id", unique: true
-    t.index [ "updated_at" ], name: "index_sessions_on_updated_at"
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,7 +67,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_105155) do
     t.string "name"
     t.string "token"
     t.datetime "updated_at", null: false
-    t.index [ "name" ], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "scores", "maps"
